28ef3e41f33d0b6972979f02e22055a298786844
Ahmed <programmingwithahmed2020@gmail.com>
2021-03-16 15:11:14 +0300

    first Commit

---
+++ app/src/main/java/com/programmingwithahmed/characterapp/MyApp.kt
@@ -0,0 +1,6 @@
+package com.programmingwithahmed.characterapp
+
+import android.app.Application
+
+class MyApp: Application() {
+}
---
+++ app/src/main/java/com/programmingwithahmed/characterapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.programmingwithahmed.characterapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.programmingwithahmed.characterapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
