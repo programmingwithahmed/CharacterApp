c01044c893a621939f9dcde849d9fc50f26dfce7
Ahmed <programmingwithahmed2020@gmail.com>
2021-03-25 23:46:34 +0300

    Add CharactersFragment to MainActivity

--- app/src/main/java/com/programmingwithahmed/characterapp/ui/MainActivity.kt
+++ app/src/main/java/com/programmingwithahmed/characterapp/ui/MainActivity.kt
@@ -2,11 +2,26 @@ package com.programmingwithahmed.characterapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.programmingwithahmed.characterapp.R
+import com.programmingwithahmed.characterapp.databinding.ActivityMainBinding
+import com.programmingwithahmed.characterapp.ui.characters.CharactersFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, CharactersFragment())
+        }
+
     }
 }
